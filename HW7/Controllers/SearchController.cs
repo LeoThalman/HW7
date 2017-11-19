f4db63fee20900d8e61535cb21f8333a4085e8eb
LeoThalman <lthalman15@wou.edu>
2017-11-18 23:37:29 -0800

    Method to pull json data from giphy now extracts fixed_height urls

--- HW7/Controllers/SearchController.cs
+++ HW7/Controllers/SearchController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HW7.Models;
 using System.Web.Mvc;
 using System.Net;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace HW7.Controllers
 {
@@ -15,19 +17,36 @@ namespace HW7.Controllers
         string APIKey = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyAPIKey"];
 
         // GET: Search
-        public async Task<JsonResult> GetData(string q)
+        public JsonResult GetData(string q)
         {
             Debug.WriteLine("sterms: " + q);
-            string qurl = "http://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + APIKey + "&limit=1";
+            string qurl = "http://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + APIKey + "&limit=2";
             WebRequest request = WebRequest.Create(qurl);
             HttpWebResponse resp = (HttpWebResponse)request.GetResponse();
-            Stream data = resp.GetResponseStream();
-            StreamReader reader = new StreamReader(data);
-            string gifdata = reader.ReadToEnd();
+            Stream dataStream = resp.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string gifData = reader.ReadToEnd();
+            Debug.WriteLine(gifData);
+            reader.Close();
+            resp.Close();
 
-            Debug.WriteLine(gifdata);
-            
-            return Json(gifdata, JsonRequestBehavior.AllowGet);
+            JObject gifs = JObject.Parse(gifData);
+            IList<JToken> data = gifs["data"].Children().Values("images").Values("fixed_height").ToList();
+            IList<ImageData> imageData = new List<ImageData>();
+            foreach(JToken gif in data)
+            {
+                ImageData image = gif.ToObject<ImageData>();
+                imageData.Add(image);
+
+            }
+            Debug.WriteLine("Test Test Test --------");
+            foreach (ImageData i in imageData)
+            {
+                Debug.WriteLine(1);
+                Debug.WriteLine(i.url);
+
+            }
+            return Json(gifData, JsonRequestBehavior.AllowGet);
         }
 
         public void TestRoute(string sterms)
---
+++ HW7/Models/ImageData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW7.Models
+{
+    public class ImageData
+    {
+        public string url { get; set; }
+    }
+}
